20f19cc392cb2719754113c2c6ea5ce8ded3d180
MarquisN <159097193+MarquisN@users.noreply.github.com>
2024-02-12 21:08:27 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,25 +1,32 @@
 # Author: Marquis Neely
 # A Receipt for my coffee shop!
-# Created the receipt 
+# Added two more itemss and thank you message
 
-# Number of coffees and muffins from the customer
-num_coffees = int(input("How many coffees? "))
-num_muffins = int(input("How many muffins? "))
+# Number of coffees, muffins, lattes and donuts from the customer
+num_coffees = int(input("1"))
+num_muffins = int(input("2"))
+num_Lattes = int(input("3"))
+num_donuts = int(input("4"))
 # Figure out the subtotal
-coffee_price = 5
-muffin_price = 4
-subtotal = (num_coffees * coffee_price) + (num_muffins * muffin_price)
+coffee_price = $5
+muffin_price = $4
+latte_price= $3
+donut_price = $2
+subtotal = $5 + $8 + $9 + $8 = $30
 # Figure out the tax (6% of the subtotal)
 tax = 0.06
-tax_amount = subtotal * tax
+tax_amount = $30 * 0.06 = $1.8
 # figure out the final price
-final_price = subtotal + tax_amount
+final_price = $30 + 1.8 = $31.8
 # Display the receipt
-print("\n***************************************")
-print("Marquis's Lattes Receipt")
-print(f"{num_coffees} Coffee $5 each: ${num_coffees * coffee_price:.2f}")
-print(f"{num_muffins} Muffins $4 each: ${num_muffins * muffin_price:.2f}")
-print(f"6% tax: ${tax_amount:.2f}")
-print("---------")
-print(f"Total: ${final_price:.2f}")
 print("***************************************")
+print("Marquis's Lattes Receipt")
+print("1 Coffee $5 each: $5")
+print("2 Muffins $4 each: $8")
+print("3 Lattes $3 each: $9")
+print("4 Donuts $2 each: $8")
+print("6% tax: $1.8")
+print("---------")
+print("Total: $31.8")
+print("***************************************")
+print("Thank you for coming to Marquis's Lattes! Pleae come again!")
