47099d453b591c555408939290c3a1faeea2e704
MarquisN <159097193+MarquisN@users.noreply.github.com>
2024-02-19 23:40:12 -0700

    Update CIS129_MarquisN_Lab4.py

--- CIS129_MarquisN_Lab4.py
+++ CIS129_MarquisN_Lab4.py
@@ -10,13 +10,13 @@ prompt =("Enter the monthy sales amount:") # prompt will be a string literal
 monthlySales = float(input("Enter the percent of increase in sales:"))
 
 # THis code determines the store bonus
-if monthlySales >= 110000
+if monthlySales >= 110000:
 storeAmount = 6000
-elif monthySales >= 100000
+elif monthySales >= 100000:
 storeAmount = 5000
-elif monthySales >= 90000
+elif monthySales >= 90000:
 storeAmount = 4000
-elif monthySales >= 80000
+elif monthySales >= 80000:
 storeAmount = 3000
 else:
 storeAmount = 0
@@ -26,17 +26,17 @@ salesIncrease = float(input("Enter the percent of increase in sales:"))
 salesIncrease = salesIncrease / 100
 
 # This code determines the empAmount bonus
-	if salesIncrease >= .05:
-		empAmount = 75
-	elif salesIncrease >= .04:
-		empAmount = 50
-	elif salesIncrease >= .03:
-		empAmount = 40
-	else:
-		empAmount = 0
+if salesIncrease >= .05:
+empAmount = 75
+elif salesIncrease >= .04:
+empAmount = 50
+elif salesIncrease >= .03:
+empAmount = 40
+else:
+empAmount = 0
 
 # This code prints the bonus information
-   print("The store bonus amount is $", storeAmount)
-   print("The employee bonus amount is $", empAmount)
-	   if (storeAmount == 6000 ) and (empAmount == 75):
-		print('Congrats! You have reached the highest bonus amounts possible! ')
+print("The store bonus amount is $", storeAmount)
+print("The employee bonus amount is $", empAmount)
+if (storeAmount == 6000 ) and (empAmount == 75):
+print('Congrats! You have reached the highest bonus amounts possible! ')
